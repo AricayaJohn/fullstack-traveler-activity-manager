1d07f51d13ae588fa25090dce98e495c8fdf3fbd
John <jaricaya@gmail.com>
2025-01-06 01:23:56 +0800

    added signup class

--- server/app.py
+++ server/app.py
@@ -11,11 +11,27 @@ from config import app, db, api, bcrypt
 from models import Traveler
 
 # Views go here!
-
 @app.route('/')
 def index():
     return '<h1>Project Server</h1>'
 
+class Signup(Resource):
+    def post(self):
+        data = request.get_json()
+
+        traveler = Traveler(
+            username = data['username'],
+            password_hash = data['password'],
+            age = data['age'],
+            email = data['email'],
+            interests = data['interests'],
+            favorite_season = data['favorite_season']
+        )
+        db.session.add(user)
+        db.session.commit()
+        return user.to_dict(), 201
+
+api.add_resource(Signup, '/signup', endpoint='signup')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
