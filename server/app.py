13d075df7502043fccdec13bb141f161d516fa40
John <jaricaya@gmail.com>
2025-01-05 22:31:56 +0800

    adjusting imports

--- server/app.py
+++ server/app.py
@@ -1,15 +1,30 @@
 #!/usr/bin/env python3
 
-# Standard library imports
-
 # Remote library imports
-from flask import request
-from flask_restful import Resource
+from flask import Flask, request
+from flask_cors import CORS
+from flask_restful import Api, Resource
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from sqlalchemy import MetaData
 
 # Local imports
-from config import app, db, api
-# Add your model imports
+from config import app, db, api, Traveler
 
+app = Flask(__name__)
+app.config.from_object('config')
+
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+
+db = SQLAlchemy(metadata=metadata)
+migrate = Migrate(app, db)
+db.init_app(app)
+
+api = Api(app)
+CORS(app)
+# Add your model imports
 
 # Views go here!
 
