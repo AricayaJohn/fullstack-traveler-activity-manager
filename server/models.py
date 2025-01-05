43a1f9cad4c363378b0a263c7aa559c11c006482
John <jaricaya@gmail.com>
2025-01-05 21:23:44 +0800

    add traveler model

--- server/models.py
+++ server/models.py
@@ -1,6 +1,20 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.ext.hybrid import hybrid_property
 
-from config import db
+from config import db, bcrypt
 
 # Models go here!
+class Traveler(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String)
+    _password_hash = db.Column(db.String)
+    age = db.Column(db.Integer)
+    email = db.Column(db.String)
+    interests = db.Column(db.String)
+    favorite_season = db.Column(db.String)
+
+
+    
