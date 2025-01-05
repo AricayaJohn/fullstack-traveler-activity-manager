56a8ab78c2756323798b06536ab703164fe29010
John <jaricaya@gmail.com>
2025-01-05 16:34:31 +0800

    initialized project setup

---
+++ server/migrations/versions/9c777ddc7f8f_create_db.py
@@ -0,0 +1,24 @@
+"""Create DB
+
+Revision ID: 9c777ddc7f8f
+Revises: 
+Create Date: 2025-01-05 16:32:59.191909
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '9c777ddc7f8f'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
