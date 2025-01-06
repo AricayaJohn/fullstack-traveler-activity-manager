2ebb558246a90229a8abb4b3748112cb2af47c1e
John <jaricaya@gmail.com>
2025-01-06 18:10:06 +0800

    added clear session, logout, check session

--- server/app.py
+++ server/app.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
 # Remote library imports
-from flask import request
+from flask import request, session
 from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Resource
 
 # Local imports
 from config import app, db, api, bcrypt
@@ -27,11 +28,51 @@ class Signup(Resource):
             interests = data['interests'],
             favorite_season = data['favorite_season']
         )
-        db.session.add(user)
+        db.session.add(traveler)
         db.session.commit()
-        return user.to_dict(), 201
+        return traveler.to_dict(), 201
 
 api.add_resource(Signup, '/signup', endpoint='signup')
+
+class Login(Resource):
+    def post(self):
+        username = request.get_json()['username']
+        password = request.get_json()['password']
+
+        traveler = Traveler.query.filter(Traveler.username == username).first()
+
+        if traveler and traveler.authenticate(password):
+            session['traveler_id'] = traveler.id
+            return traveler.to_dict(), 200
+        return {'error': 'Unauthorized log in'}, 401
+
+api.add_resource(Login, '/login', endpoint='/login')
+
+class ClearSession(Resource):
+    def delete(self):
+        session['page_views'] = None
+        session['traveler_id'] = None
+
+        return {}, 204
+
+api.add_resource(ClearSession, '/clear', endpoint='clear')
+
+class CheckSession(Resource):
+    def get(self):
+        if session.get('traveler_id'):
+            traveler = Traveler.query.filter(Traveler.id == session['traveler_id']).first()
+            return traveler.to_dict(), 200
+        return {}, 204
+
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+
+class Logout(Resource):
+    def delete(self):
+        session['traveler_id'] = None
+        return {}, 204
+
+api.add_resource(Logout, '/logout', endpoint='logout')
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
