c98e3500482529e08e511dd77f42ca0666ef05f6
John <jaricaya@gmail.com>
2025-01-07 20:31:51 +0800

    added traveler seed

--- server/seed.py
+++ server/seed.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python3
-from random import randint, choice as rc
-from faker import Faker
 from app import app
-from models import db
+from models import db, Traveler, Activity, Destination
+from config import bcrypt
+
+def clear_data():
+    Traveler.query.delete()
+    Activity.query.delete()
+    Destination.query.delete()
+    db.session.commit()
+
+def add_travelers():
+    traveler1 = Traveler(username='john', password_hash = pass123, age=28, email='john@example.com', interest='Hiking', favorite_season='Summer' )
+    traveler2 = Traveler(username='jane', password_hash = pass456, age=24, email='jane@example.com', interest='Snowboarding', favorite_season='Winter')
+
+    db.session.add_all([Traveler1, Traveler2])
+    db.session.commit()
+
+
 
 if __name__ == '__main__':
     fake = Faker()
