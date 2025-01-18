49dcf4dc47a5af04aa3db1572497240975904fff
John <jaricaya@gmail.com>
2025-01-18 15:48:57 +0800

    started editpage

---
+++ client/src/components/EditPage.js
@@ -0,0 +1,22 @@
+import React, {useEffect, useState} from 'react';
+
+function EditTripForm({ tripId, onRedirect }) {
+    const [trip, setTrip] = useState(null);
+
+    useEffect(() => {
+        fetch(`/activities/${tripId}`).then((r) => {
+            if (r.ok) {
+                r.json().then((data) => {
+                    setTrip(data);
+                });
+            }
+        });
+    }, [tripId]);
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+
+        })
+    }
+}
