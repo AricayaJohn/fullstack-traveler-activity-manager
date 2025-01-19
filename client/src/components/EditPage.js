e2c7f28280b205bc6e526f03bcd156b619e4b35b
John <jaricaya@gmail.com>
2025-01-19 19:30:21 +0800

    added form and buttons for edit page

--- client/src/components/EditPage.js
+++ client/src/components/EditPage.js
@@ -36,4 +36,90 @@ function EditTripForm({ tripId, onRedirect }) {
             }
         });
     };
+
+    if (!trip) 
+        return <div>Loading</div>
+    
+    return (
+        <form onSubmit={handleSubmit}>
+            <h2> Edit Trip </h2>
+            <div>
+                <label> Activity Name: </label>
+                <input 
+                    type = "text"
+                    value = {trip.activity_name}
+                    onChange = {(e) => setTrip({ ...trip, activity_name: e.target.value })}
+                    required
+                />
+            </div>
+            <div>
+                <label> Difficulty</label>
+                <input 
+                    type = "text"
+                    value = {trip.difficulty}
+                    onChange = {(e) => setTrip({ ...trip, difficulty: e.target.value })}
+                />
+            </div>
+            <div>
+                <label> Season: </label>
+                <input 
+                    type = "text"
+                    value = {trip.season_for_activity}
+                    onChange = {(e) => setTrip({ ...trip, season_for_activity: e.target.value })}
+                />
+            </div>
+            <div>
+                <label> Duration: </label>
+                <input 
+                    type = "text"
+                    value = {trip.duration}
+                    onChange = {(e) => setTrip({ ...trip, duration: e.target.value })}
+                />
+            </div>
+            <div>
+                <label> Price: </label>
+                <input 
+                    type = "text"
+                    value = {trip.price}
+                    onChange= {(e) => setTrip({ ...trip, price: e.target.value})}
+                />
+            </div>
+            <div>
+                <label> Price: </label>
+                <input 
+                    type = "text"
+                    value = {trip.price}
+                    onChange= {(e) => setTrip({ ...trip, price: e.target.value})}
+                />
+            </div>
+            <div>
+                <label> Destination Name: </label>
+                <input 
+                    type = "text"
+                    value = {trip.destination.name}
+                    onChange= {(e) => setTrip({ ...trip, destination: { ...trip.destination, name: e.target.value} })}
+                />
+            </div>
+            <div>
+                <label> transportation: </label>
+                <input 
+                    type = "text"
+                    value = {trip.transportation}
+                    onChange= {(e) => setTrip({ ...trip, destination: { ...trip.destination, transportation: e.target.value} })}
+                />
+            </div>
+            <div>
+                <label> country: </label>
+                <input 
+                    type = "text"
+                    value = {trip.country}
+                    onChange= {(e) => setTrip({ ...trip, destination: { ...trip.destination, country: e.target.value} })}
+                />
+            </div>
+            <button type = "submit"> Submit Changes</button>
+            <button type = "button" onClick = {handleDelete}>Delete Activity</button>
+        </form>
+    );
 }
+
+export default EditTripForm
